613d36f9f2c18d9cabdd6672cab8315800215f11
unknown <ogbonnayakingsike@gmail.com>
2023-04-14 23:57:59 -0700

    hosting

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,16 +3,17 @@ import * as config from 'config'
 import { UserEntity } from "src/auth/user.entity";
 import { TaskEntity } from "src/tasks/task.entity";
 
+require('dotenv').config()
 
 const dbConfig = config.get('db')
 export const typeOrmConfig: TypeOrmModuleOptions = {
 
     type: dbConfig.type,
-    host: process.env.RDS_HOSTNAME || dbConfig.host,
-    port: process.env.RDS_PORT || dbConfig.port,
-    username: process.env.RDS_USERNAME || dbConfig.username,
-    password: process.env.RDS_PASSWORD || dbConfig.password,
-    database: process.env.RDS_DB_NAME || dbConfig.database,
+    host: process.env.DB_HOSTNAME || dbConfig.host,
+    port: process.env.DB_PORT || dbConfig.port,
+    username: process.env.DB_USERNAME || dbConfig.username,
+    password: process.env.DB_PASSWORD || dbConfig.password,
+    database: process.env.DB_NAME || dbConfig.database,
     entities: [TaskEntity, UserEntity],
     synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
     autoLoadEntities: dbConfig.autoLoadEntities,
